ceb74de1d03fcbf476bb8145df6582fb6d490238
Otto Oikarinen <80541689+OttoOikarinen@users.noreply.github.com>
2024-12-19 19:34:38 +0200

    Added hello-route.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,5 +4,9 @@ import {Request, Response, Router} from "express"
 
 const router: Router = Router()
 
+router.get("/hello", (req: Request, res: Response) => {
+    res.status(200).send("Hello World!");
+})
+
 
 export default router
